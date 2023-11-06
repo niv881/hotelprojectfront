c97cd41f3463ba11d532761ae8761431eec6f9e4
nivHason <nivhason881@gmail.com>
2023-11-06 22:09:23 +0200

    delete styles problems

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -26,7 +26,7 @@ const RoomCard = ({
                 >
                   <Link
                     to={`/room/${hotelName}/${room.type}/${dateCheckIn}/${dateCheckOut}/${room.price}`}
-                    className="group overflow-hidden relative after:duration-500 before:duration-500  duration-500 hover:after:duration-500 hover:after:translate-x-16 hover:before:translate-y-8 hover:before:-translate-x-16 hover:duration-500 after:absolute after:w-16 after:h-16 after:bg-sky-700 after:rounded-full after:blur-xl after:bottom-16 after:right-8 after:w-8 after:h-8 before:absolute before:w-12 before:h-12 before:bg-sky-400 before:rounded-full before:blur-xl before:top-8 before:right-8 before:w-8 before:h-8 hover:rotate-12 flex justify-center items-center h-32 w-48 origin-bottom-right bg-neutral-900 rounded-2xl outline outline-slate-400 -outline-offset-4"
+                    className="group overflow-hidden relative after:duration-500 before:duration-500  duration-500 hover:after:duration-500 hover:after:translate-x-16 hover:before:translate-y-8 hover:before:-translate-x-16 hover:duration-500 after:absolute  after:bg-sky-700 after:rounded-full after:blur-xl after:bottom-16 after:right-8 after:w-8 after:h-8 before:absolute  before:bg-sky-400 before:rounded-full before:blur-xl before:top-8 before:right-8 before:w-8 before:h-8 hover:rotate-12 flex justify-center items-center h-32 w-48 origin-bottom-right bg-neutral-900 rounded-2xl outline outline-slate-400 -outline-offset-4"
                   >
                     <div className="z-10 flex flex-col items-center gap-2">
                       <span className="text-slate-400 font-bold text-base">
